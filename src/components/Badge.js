5236147a1f6cd43657cffd78e009f685d46dfe53
Cristian Pérez <crperz@outlook.com>
2020-01-13 16:51:25 -0500

    Add styles to Badge component

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,27 +1,32 @@
 import React from 'react';
 
+import './styles/Badge.css';
 import confLogo from '../images/badge-header.svg';
 
 const Badge = () => {
   return (
-    <div>
-      <div>
+    <div className="Badge">
+      <div className="Badge__header">
         <img src={confLogo} alt="Conf Logo" />
       </div>
 
-      <div>
-        <img src="https://www.gravatar.com/avatar/HASH" alt="Avatar" />
+      <div className="Badge__section-name">
+        <img
+          className="Badge__avatar"
+          src="https://www.gravatar.com/avatar/HASH"
+          alt="Avatar"
+        />
         <h1>
           Cristian <br /> Pérez
         </h1>
       </div>
 
-      <div>
-        <p>Fullstack Developer</p>
-        <p>@crperz</p>
+      <div className="Badge__section-info">
+        <h3>Fullstack Developer</h3>
+        <div>@crperz</div>
       </div>
 
-      <div>#reactconf</div>
+      <div className="Badge__footer">#reactconf</div>
     </div>
   );
 };
